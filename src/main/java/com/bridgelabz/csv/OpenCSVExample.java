9b8fd9bbe8957463407f1ad687cad8134f46d647
mahendrakundare <mr.kundare@gmail.com>
2019-11-29 09:25:58 +0530

    CSV record fetched using String array

---
+++ src/main/java/com/bridgelabz/csv/OpenCSVExample.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.csv;
+
+
+import com.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/*
+Retrive each record as string array
+ */
+public class OpenCSVExample {
+    private static final String CSV_PATH = "/home/admin1/IdeaProjects/CSV-Example/user.csv";
+
+    public static void main(String[] args) throws IOException {
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH));
+                CSVReader csvReader = new CSVReader(reader);
+        ) {
+            // Reading Records One by One in a String array
+            String[] nextRecord;
+            while ((nextRecord = csvReader.readNext()) != null) {
+                System.out.println("Name : " + nextRecord[0]);
+                System.out.println("Email : " + nextRecord[1]);
+                System.out.println("Phone : " + nextRecord[2]);
+                System.out.println("Country : " + nextRecord[3]);
+                System.out.println("==========================");
+            }
+        }
+    }
+}
