357f1c8acf86610bcf4f6b320b2b7cdb90acbc71
mahendrakundare <mr.kundare@gmail.com>
2019-11-29 09:52:06 +0530

    record fetched by skipping header line

--- src/main/java/com/bridgelabz/csv/OpenCSVExample.java
+++ src/main/java/com/bridgelabz/csv/OpenCSVExample.java
@@ -2,6 +2,7 @@ package com.bridgelabz.csv;
 
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,12 +14,14 @@ import java.nio.file.Paths;
 Retrive each record as string array
  */
 public class OpenCSVExample {
-    private static final String CSV_PATH = "/home/admin1/IdeaProjects/CSV-Example/user.csv";
+    private static final String CSV_PATH = "/home/admin1/IdeaProjects/CSV-Example/user-with-header.csv";
 
     public static void main(String[] args) throws IOException {
         try (
                 Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH));
-                CSVReader csvReader = new CSVReader(reader);
+                // CSVReader csvReader = new CSVReader(reader);
+                //for skip header row from the file
+                CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
         ) {
             // Reading Records One by One in a String array
             String[] nextRecord;
