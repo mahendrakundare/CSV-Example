8901c9c4bd99ad6c90704e8e00b5ec35f4617d01
mahendrakundare <mr.kundare@gmail.com>
2019-11-29 11:07:55 +0530

    write into csv file implemented using opencsv

---
+++ src/main/java/com/bridgelabz/csv/OpenCSVWriter.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.csv;
+
+import com.bridgelabz.csv.MyUser;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+public class OpenCSVWriter {
+    private static final String SAMPLE_CSV_FILE_PATH = "./writeCsv.csv";
+
+    public static void main(String[] args) throws IOException {
+        try (
+                Writer writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE_PATH));
+
+                CSVWriter csvWriter = new CSVWriter(writer,
+                        CSVWriter.DEFAULT_SEPARATOR,
+                        CSVWriter.NO_QUOTE_CHARACTER,
+                        CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                        CSVWriter.DEFAULT_LINE_END);
+        ) {
+            String[] headerRecord = {"Name", "Email", "Phone", "Country"};
+            csvWriter.writeNext(headerRecord);
+
+            csvWriter.writeNext(new String[]{"Sundar Pichai ♥", "sundar.pichai@gmail.com", "+1-1111111111", "India"});
+            csvWriter.writeNext(new String[]{"Satya Nadella", "satya.nadella@outlook.com", "+1-1111111112", "India"});
+        }
+    }
+}
